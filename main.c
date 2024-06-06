2c7651054c64aa2f5cf2e25660cfb373e645c2bf
Ashley Collinge <me@ashleycollinge.co.uk>
2024-06-06 20:26:18 +0100

    Variables types

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
+/*  Name: main.c
+    Purpose: Learning application
+    Author: Ashley Collinge
+*/
 #include <stdio.h>
 
 int main(void)
 {
     /* This is a comment */
     printf("To C or not to C: that is the question.\n");
+
+
+    int height;
+    float profit;
+
+    height = 10;
+    profit = 230.10;
+
+    printf("Height: %d\n", height);
+    printf("Profit: %f\n", profit);
+
     return 0;
 }
